9ab5418a9145486f0a8dd5125502ec9b41c8d385
v0lt <mpc-be@mail.ru>
2020-05-14 21:15:15 +0300

    VapourSynth: корректировка bmiHeader.biBitCount (например для YV12 нужно писать 12, а не 8), без которой MPC VR создает буфер меньшего размера. TODO: разобраться, почему MPC VR учитывает biBitCount для такого случая.

--- Source/VapourSynthStream.cpp
+++ Source/VapourSynthStream.cpp
@@ -155,7 +155,7 @@ CVapourSynthStream::CVapourSynthStream(const WCHAR* name, CSource* pParent, HRES
 		vih2->bmiHeader.biWidth       = m_PitchBuff / m_Format.Packsize;
 		vih2->bmiHeader.biHeight      = (m_Format.fourcc == BI_RGB) ? -(long)m_Height : m_Height;
 		vih2->bmiHeader.biPlanes      = 1;
-		vih2->bmiHeader.biBitCount    = m_vsInfo->format->bitsPerSample;
+		vih2->bmiHeader.biBitCount    = m_Format.CDepth * m_Format.buffCoeff / 2;
 		vih2->bmiHeader.biCompression = m_Format.fourcc;
 		vih2->bmiHeader.biSizeImage   = m_BufferSize;
 
