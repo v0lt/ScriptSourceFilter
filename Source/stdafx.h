1854f6342c6f37f39d85979f6e9bb8ddfea51bfa
v0lt <mpc-be@mail.ru>
2021-02-08 19:29:42 +0300

    Используем внешний подмодуль BaseClasses. https://github.com/v0lt/BaseClasses.git

--- Source/stdafx.h
+++ Source/stdafx.h
@@ -1,5 +1,5 @@
 /*
- * (C) 2020 see Authors.txt
+ * (C) 2020-2021 see Authors.txt
  *
  * This file is part of MPC-BE.
  *
@@ -39,5 +39,5 @@
 #include <vector>
 #include <exception>
 
-#include "../BaseClasses/streams.h"
+#include "../external/BaseClasses/streams.h"
 #include "../external/fmt/include/fmt/format.h"
