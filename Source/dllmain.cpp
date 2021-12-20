471b02516862664a50e41753b8a79e97d8a0980c
v0lt <mpc-be@mail.ru>
2021-12-20 20:23:11 +0300

    Убрана регистрация фильтра для расширений .avs и .vpy, т.к. перестает работать AVI/WAV File Source, который был указан инсталляторами AviSynth+ и VapourSynth. Лучше использовать ScriptSourceFilter как внешний фильтр в MPC-BE, MPC-HC и поднимать приоритет при необходимости.

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -51,24 +51,6 @@ int g_cTemplates = std::size(g_Templates);
 
 STDAPI DllRegisterServer()
 {
-	LPCWSTR strGuid = L"{7D3BBD5A-880D-4A30-A2D1-7B8C2741AFEF}";
-	const DWORD cbData = (DWORD)(wcslen(strGuid) + 1) * sizeof(WCHAR);
-
-	HKEY hKey;
-	LONG ec = ::RegCreateKeyExW(HKEY_CLASSES_ROOT, L"Media Type\\Extensions\\.avs", 0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &hKey, 0);
-	if (ec == ERROR_SUCCESS) {
-		ec = ::RegSetValueExW(hKey, L"Source Filter", 0, REG_SZ,
-			reinterpret_cast<const BYTE*>(strGuid), cbData);
-		::RegCloseKey(hKey);
-	}
-
-	ec = ::RegCreateKeyExW(HKEY_CLASSES_ROOT, L"Media Type\\Extensions\\.vpy", 0, 0, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, 0, &hKey, 0);
-	if (ec == ERROR_SUCCESS) {
-		ec = ::RegSetValueExW(hKey, L"Source Filter", 0, REG_SZ,
-			reinterpret_cast<const BYTE*>(strGuid), cbData);
-		::RegCloseKey(hKey);
-	}
-
 	return AMovieDllRegisterServer2(TRUE);
 }
 
