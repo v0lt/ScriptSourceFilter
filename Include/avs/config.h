dbd6a59b8fe90cc1c22022739d5184229281c23d
v0lt <mpc-be@mail.ru>
2022-08-12 21:08:46 +0300

    Обновлены файлы AviSynth+ 3.7.2.

--- Include/avs/config.h
+++ Include/avs/config.h
@@ -48,6 +48,19 @@
 #   define X86_64
 #elif defined(_M_IX86) || defined(__i386__)
 #   define X86_32
+// VS2017 introduced _M_ARM64
+#elif defined(_M_ARM64) || defined(__aarch64__)
+#   define ARM64
+#elif defined(_M_ARM) || defined(__arm__)
+#   define ARM32
+#elif defined(__PPC64__)
+#   define PPC64
+#elif defined(_M_PPC) || defined(__PPC__) || defined(__POWERPC__)
+#   define PPC32
+#elif defined(__riscv)
+#   define RISCV
+#elif defined(__sparc_v9__)
+#   define SPARC
 #else
 #   error Unsupported CPU architecture.
 #endif
@@ -75,6 +88,11 @@
 #elif defined(__GNUC__)
 #   define GCC
 #   define AVS_FORCEINLINE __attribute__((always_inline)) inline
+#elif defined(__INTEL_COMPILER) || defined(__INTEL_LLVM_COMPILER)
+// Intel C++ Compilers with MSVC command line interface will not appear here rather at _MSC_VER
+#   define AVS_FORCEINLINE inline
+#   undef __forceinline
+#   define __forceinline inline
 #else
 #   error Unsupported compiler.
 #   define AVS_FORCEINLINE inline
@@ -93,12 +111,61 @@
 #elif defined(__APPLE__)
 #   define AVS_MACOS
 #   define AVS_POSIX
+#elif defined(__HAIKU__)
+#   define AVS_HAIKU
+#   define AVS_POSIX
 #else
 #   error Operating system unsupported.
 #endif
 
-#if defined(AVS_POSIX)
-#define NEW_AVSVALUE
+// useful warnings disabler macros for supported compilers
+
+#if defined(_MSC_VER)
+#define DISABLE_WARNING_PUSH           __pragma(warning( push ))
+#define DISABLE_WARNING_POP            __pragma(warning( pop ))
+#define DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
+
+#define DISABLE_WARNING_UNREFERENCED_LOCAL_VARIABLE      DISABLE_WARNING(4101)
+#define DISABLE_WARNING_UNREFERENCED_FUNCTION            DISABLE_WARNING(4505)
+// other warnings you want to deactivate...
+
+#elif defined(__GNUC__) || defined(__clang__)
+#define DO_PRAGMA(X) _Pragma(#X)
+#define DISABLE_WARNING_PUSH           DO_PRAGMA(GCC diagnostic push)
+#define DISABLE_WARNING_POP            DO_PRAGMA(GCC diagnostic pop)
+#define DISABLE_WARNING(warningName)   DO_PRAGMA(GCC diagnostic ignored #warningName)
+
+#define DISABLE_WARNING_UNREFERENCED_LOCAL_VARIABLE      DISABLE_WARNING(-Wunused-variable)
+#define DISABLE_WARNING_UNREFERENCED_FUNCTION            DISABLE_WARNING(-Wunused-function)
+// other warnings you want to deactivate...
+
+#else
+#define DISABLE_WARNING_PUSH
+#define DISABLE_WARNING_POP
+#define DISABLE_WARNING_UNREFERENCED_LOCAL_VARIABLE
+#define DISABLE_WARNING_UNREFERENCED_FUNCTION
+// other warnings you want to deactivate...
+
+#endif
+
+#if defined(AVS_WINDOWS) && defined(_USING_V110_SDK71_)
+// Windows XP does not have proper initialization for
+// thread local variables.
+// Use workaround instead __declspec(thread)
+#define XP_TLS
+#endif
+
+#ifndef MSVC
+// GCC and Clang can be used on big endian systems, MSVC can't.
+#  if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#    define AVS_ENDIANNESS "little"
+#  elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#    define AVS_ENDIANNESS "big"
+#  else
+#    define AVS_ENDIANNESS "middle"
+#  endif
+#else
+#define AVS_ENDIANNESS "little"
 #endif
 
 #endif //AVS_CONFIG_H
